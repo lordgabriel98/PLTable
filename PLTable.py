25f05d9a748bdead605752410f447da831c7f9ae
lordgabriel98 <bajegabriel@gmail.com>
2025-06-29 09:51:16 +1000

    `Pos` column set as index
    default index that starts at 0 is removed

--- PLTable.py
+++ PLTable.py
@@ -4,17 +4,20 @@ from bs4 import BeautifulSoup  # to traverse the website's html
 import pandas as pd # to convert the html to a dataframe
 
 url = 'https://www.premierleague.com/'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
-table = soup.find('div', {'class': 'tablesContainer'})
+try:
+    page = requests.get(url)
+except Exception as err:
+    print(err)
 
-rows = table.find_all('tr')
-
-df = pd.read_html(str(table))[0]
-
-df["Pos"] = df["Pos"].apply(lambda x: x[:2])
-
-print(df)
+else:
+    soup = BeautifulSoup(page.text, 'html.parser')
+    table = soup.find('div', {'class': 'tablesContainer'})
+    rows = table.find_all('tr')
+finally:
+    df = pd.read_html(str(table))[0]
+    df.set_index("Pos", inplace=True)
+    
+    print(df)
 
 
 #YNWA
