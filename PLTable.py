69c0c7a9cb2671acbd2a74a828414ce5ba88e628
lordgabriel98 <bajegabriel@gmail.com>
2025-06-29 11:53:45 +1000

    Added error handling for requests package

--- PLTable.py
+++ PLTable.py
@@ -6,18 +6,19 @@ import pandas as pd # to convert the html to a dataframe
 url = 'https://www.premierleague.com/'
 try:
     page = requests.get(url)
-except Exception as err:
-    print(err)
-
-else:
     soup = BeautifulSoup(page.text, 'html.parser')
     table = soup.find('div', {'class': 'tablesContainer'})
-    rows = table.find_all('tr')
-finally:
-    df = pd.read_html(str(table))[0]
-    df.set_index("Pos", inplace=True)
     
-    print(df)
+    if(table):
+        rows = table.find_all('tr')
+        df = pd.read_html(str(table))[0]
+        df.set_index("Pos", inplace=True)
+        print(df)
+    else:
+        print("Resource could not be scraped from website.")
+        
+except requests.exceptions.RequestException as e:
+    print("Request error: ", e)
 
 
 #YNWA
