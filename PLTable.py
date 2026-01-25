d07f3bbfe67749d4b803275696a1f46642432584
lordgabriel98 <bajegabriel@gmail.com>
2026-01-25 20:09:14 +1000

    Added error handling for no/bad Internet cases

--- PLTable.py
+++ PLTable.py
@@ -7,10 +7,6 @@ import pandas as pd # to convert the html to a dataframe
 
 api = 'https://sdp-prem-prod.premier-league-prod.pulselive.com/api/v5/competitions/8/seasons/2025/standings?live=false'
 
-results = requests.get(api)
-
-api_data = results.json()
-
 team_names = []
 
 team_positions = []
@@ -25,23 +21,30 @@ drawn = []
 
 lost = []
 
-for data in api_data['tables']:
-    
-    for entry in data['entries']:
-        team_data = entry['overall']
-        del team_data['startingPosition']
-        team_name = entry['team']
-        team_names.append(team_name['name'])
-        team_positions.append(team_data['position'])
-        points.append(team_data['points'])
-        played.append(team_data['played'])
-        won.append(team_data['won'])
-        drawn.append(team_data['drawn'])
-        lost.append(team_data['lost'])
+try:
+    results = requests.get(api)
+    api_data = results.json()
 
-table = pd.DataFrame({"Position": team_positions, "Club": team_names, 'Points': points, 'Played': played,
-                      "Won": won, "Draws": drawn, "Lost": lost})
-table.set_index('Position', inplace=True)
-print(table)
+    for data in api_data['tables']:
+    
+        for entry in data['entries']:
+            team_data = entry['overall']
+            del team_data['startingPosition']
+            team_name = entry['team']
+            team_names.append(team_name['name'])
+            team_positions.append(team_data['position'])
+            points.append(team_data['points'])
+            played.append(team_data['played'])
+            won.append(team_data['won'])
+            drawn.append(team_data['drawn'])
+            lost.append(team_data['lost'])
+
+    table = pd.DataFrame({"Position": team_positions, "Club": team_names, 'Points': points, 'Played': played,
+                        "Won": won, "Draws": drawn, "Lost": lost})
+    table.set_index('Position', inplace=True)
+    print(table)
+
+except requests.exceptions.ConnectionError:
+    print("Please check your Internet connection.")
 
 #YNWA
